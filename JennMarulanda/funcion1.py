8bce9206650966db98f05ff3bbb07756506112af
JennMar <marula9205@hotmail.com>
2022-09-24 11:37:36 -0500

    Se crea función saludar

--- JennMarulanda/funcion1.py
+++ JennMarulanda/funcion1.py
@@ -1,2 +1,13 @@
 #Funciones tradicionales con Python
 
+#Declaración de una función:
+from json import JSONEncoder
+
+
+def saludar(nombre):
+    print(f'Hola, {nombre}')
+    
+#Invocar la función:
+nombre = "Jenn"
+saludar(nombre)
+
